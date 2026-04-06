e48428bb8e5ea6b2670683042adf4f234cb813d4
Alex Zubkov <zas1608@gmail.com>
2026-04-06 17:39:57 +0300

    1

--- 3/z.sql
+++ 3/z.sql
@@ -2,4 +2,17 @@
 SELECT sum(p.weight) FROM rooms r 
   JOIN racks ra ON ra.room_id = r.id
   JOIN storages s ON s.shelf_id = ra.id
-  JOIN products p ON p.storage_id = s.id
+  JOIN products p ON p.storage_id = s.id;
+
+-- 1
+WITH cl_sh as (
+  SELECT sum(p.height * p.width * p.length), c.client_id FROM rooms r 
+    JOIN racks ra ON ra.room_id = r.id
+    JOIN storages s ON s.shelf_id = ra.id
+    JOIN products p ON p.storage_id = s.id
+    JOIN contracts c ON c.client_id = p.contract_id
+    GROUP BY c.client_id
+)
+
+SELECT * FROM cl_sh
+LIMIT 3;
